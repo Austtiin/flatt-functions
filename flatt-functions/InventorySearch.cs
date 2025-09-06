e41de59b7aa382838ad4eaa901d5a0ca2bba35fb
Austtiin <austinstephens103@gmail.com>
2025-09-05 22:34:20 -0500

    Add initial Azure Functions project setup
    Set up a new Azure Functions project targeting .NET 8.0 with Application Insights integration. Added a sample HTTP-triggered function (InventorySearch), project configuration files, and recommended VS Code extension.

---
+++ flatt-functions/InventorySearch.cs
@@ -0,0 +1,23 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace flatt_functions;
+
+public class InventorySearch
+{
+    private readonly ILogger<InventorySearch> _logger;
+
+    public InventorySearch(ILogger<InventorySearch> logger)
+    {
+        _logger = logger;
+    }
+
+    [Function("InventorySearch")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        return new OkObjectResult("Welcome to Azure Functions!");
+    }
+}
